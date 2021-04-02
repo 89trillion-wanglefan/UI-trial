84204521b656fa297f9b49566f9f05c087243133
89trillion-wanglefan <lefan@89trillion.com>
2021-04-02 16:03:15 +0800

    优化屏幕适配，添加独立类

---
+++ Assets/Scripts/ScreeAdjust.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreeAdjust : MonoBehaviour
+{
+    public List<CanvasScaler> canvass=new List<CanvasScaler>();//涉及屏幕适配的所有画布
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        MatchScreen();
+    }
+
+    /// <summary>
+    /// 调整屏幕适配的方案，根据用户屏幕宽高比调整适配方向
+    /// </summary>
+    private void MatchScreen()
+    {
+        if (Screen.width / (float)Screen.height > 1080 / (float)1920)
+        {
+            foreach (var t in canvass)
+            {
+                t.matchWidthOrHeight = 1;
+            }
+        }
+        else
+        {
+            foreach (var t in canvass)
+            {
+                t.matchWidthOrHeight = 0;
+            }
+        }
+    }
+}
